559aaa4855d3701e7eb469cfca63fe01bf5e23d5
paulo branco <paulorobertobranco@gmail.com>
2019-04-23 15:32:33 -0300

    flask files

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-test
+from flask import Flask, render_template, request
+import googlemaps
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template('index.html', app_name="- PHARMACY -")
+
+
+if __name__ == "__main__":
+
+	app.run()
